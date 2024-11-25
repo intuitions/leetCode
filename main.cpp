da3ad88d4ec5718d6c4d19edcc5e7721d06b60d2
intuitions <tdcq16768@qq.com>
2024-11-25 11:51:49 +0000

    add i/o testing functions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+    int x = 0;
+    cout << "please input a integer:" << endl;
+    cin >> x;
+    cout << "x:" << x << endl;
     cout << "hello world!" << endl;
     return 0;
 }
